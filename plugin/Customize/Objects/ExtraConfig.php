f9b137f227ccaf50f6b1cf69dbeba6c0fb17d210
jeffreyniu <niutianbo@hotmail.com>
2024-07-24 17:59:42 +0800

    add JEFNIU_Customize plugin to plugins table.

--- plugin/Customize/Objects/ExtraConfig.php
+++ plugin/Customize/Objects/ExtraConfig.php
@@ -3,13 +3,24 @@
 global $global;
 require_once $global['systemRootPath'] . 'plugin/Plugin.abstract.php';
 
+$sql = "INSERT INTO `plugins` VALUES (NULL, 'c4fe1b83-8f5a-4d1b-b912-172c608bf9e3', 'active', now(), now(), '".json_encode($object_data)."', 'JEFNIU_Customize', 'JEFNIU_Customize', '1.0');";
+try {
+    $mysqli->query($sql);
+} catch (Exception $exc) {
+    error_log("Installation: ".__LINE__." Error creating JEFNIU_Customize plugin data: " . $mysqli->error);
+}
+$mysqli->close();
+
 class ExtraConfig extends PluginAbstract
 {
     public function getTags()
     {
         return [
+            PluginTags::$RECOMMENDED,
             PluginTags::$FREE,
             PluginTags::$MOBILE,
+            PluginTags::$LAYOUT,
+
         ];
     }
 
